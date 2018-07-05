721be736f805fc25ca60b06363531bc79fab7286
kevzter18 <kevzter18@gmail.com>
2018-07-05 13:35:25 +0800

    Add files via upload

--- Timeslot.java
+++ Timeslot.java
@@ -10,5 +10,31 @@ package geneticalgorithmdemo;
  * @author Kevin James T Kramer
  */
 public class Timeslot {
+    //the Timeslot class has 2 fields
+    public int day;
+    public int time;
     
+    // the Timeslot class has one constructor
+    public Timeslot(int nDay, int nTime) {
+        day = nDay;
+        time = nTime;
+        
+    }
+        
+    // the Timeslot class has four methods
+    public void setDay(int newValue) {
+      day = newValue;
+    }
+        
+    public void setTime(int newValue) {
+      time = newValue;
+    }
+        
+    public int getDay() {
+    return day;
+    }
+        
+    public int getTime() {
+    return time;
+    }
 }
