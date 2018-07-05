240777527c589f2caae546aed9fe086208043681
kevzter18 <kevzter18@gmail.com>
2018-07-05 22:12:27 +0800

    added the methods

--- GeneticAlgorithmDemo/src/geneticalgorithmdemo/GeneticAlgorithmDemo.java
+++ GeneticAlgorithmDemo/src/geneticalgorithmdemo/GeneticAlgorithmDemo.java
@@ -19,11 +19,17 @@ public class GeneticAlgorithmDemo {
      */
     public static void main(String[] args) {
         
-        //initializing Faculty members
+        //Initializing Faculty members
         ArrayList<Faculty> facultyList = new ArrayList<Faculty>();
         
         Faculty professorA = new Faculty(151, "Solomon See", 12);
+        Faculty professorB = new Faculty(150, "Ryan Dimaunahan", 9);
+        Faculty professorC = new Faculty(101, "Rafael Cabredo", 6);
         
+        facultyList.add(professorA);
+        facultyList.add(professorB);
+        facultyList.add(professorC);
+        //End of Faculty initialization
         
         
         ArrayList<Course> courseList = new ArrayList<Course>();
@@ -31,4 +37,17 @@ public class GeneticAlgorithmDemo {
         
     }
     
+    public double fitnessFunction(ArrayList<Offering> member){
+       double test =  50.4;
+        return test;
+    }
+    public void sortPopulation (ArrayList<ArrayList<Offering>> population){
+        
+    }
+    
+    public void selectParent (ArrayList<ArrayList<Offering>> population){
+        //made it void so it wouldn't be annoying 
+    }
+    
+  
 }
